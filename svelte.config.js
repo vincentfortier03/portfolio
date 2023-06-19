dc80feba23c67c25ff2954dbd6ae0e25800e3bc0
vincentfortier03 <vincentfortier03@gmail.com>
2023-06-18 21:58:16 -0400

    kit?

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import seqPreprocessor from 'svelte-sequential-preprocessor'
 import { preprocessThrelte } from '@threlte/preprocess'
 
 
-/** @type {import('@sveltejs/adapter-vercel').Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
